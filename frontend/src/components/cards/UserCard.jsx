ebc9ae6b7375fbf345e001eae0d3a3e0c40c9e54
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-05-24 16:35:14 +0530

    Update UserCard.jsx

--- frontend/src/components/cards/UserCard.jsx
+++ frontend/src/components/cards/UserCard.jsx
@@ -17,11 +17,11 @@ const UserCard = () => {
   return (
     user && (
     <div className='flex gap-1 w-full  items-center'>
-        <div className=' '>
+        <div className='lg:w-1/3 '>
             <img className='h-14 w-14 rounded-full object-cover '      src={user.profileImageUrl}  />
         </div>
-        <div className='flex flex-col items-center w-full'> 
-        <div className='text-xl min-w-[20%] flex item-center justify-center font-semibold font-sans  text-blue-600 hover:cursor-pointer hover:text-blue-800'>
+        <div className='flex flex-col w-1/2 '> 
+        <div className='text-xl min-w-[20%] min-w-40  flex item-center justify-center font-semibold font-sans  text-blue-600 hover:cursor-pointer hover:text-blue-800'>
             @{user.name || ""} 
             
         </div>
