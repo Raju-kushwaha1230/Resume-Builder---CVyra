687338abd341e2be5350ee485fd2d9ad72cf4e4c
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-05-24 16:23:42 +0530

    Update UserCard.jsx

--- frontend/src/components/cards/UserCard.jsx
+++ frontend/src/components/cards/UserCard.jsx
@@ -21,7 +21,7 @@ const UserCard = () => {
             <img className='h-14 w-14 rounded-full object-cover '      src={user.profileImageUrl}  />
         </div>
         <div className='flex flex-col items-center'> 
-        <div className='text-xl min-w-[40px] font-semibold font-sans  text-blue-600 hover:cursor-pointer hover:text-blue-800'>
+        <div className='text-xl min-w-[14%] flex item-center justify-center font-semibold font-sans  text-blue-600 hover:cursor-pointer hover:text-blue-800'>
             @{user.name || ""} 
             
         </div>
